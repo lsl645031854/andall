47dfff787fb464db4347455156eb6f658f208668
shuailing.li <shuailing.li@andall.com>
2020-03-11 15:24:48 +0800

    测试回滚

--- src/main/java/com/andall/sally/supply/service/impl/MsgServiceImpl.java
+++ src/main/java/com/andall/sally/supply/service/impl/MsgServiceImpl.java
@@ -3,7 +3,6 @@ package com.andall.sally.supply.service.impl;
 import com.andall.sally.supply.constant.MqConstant;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Service;
