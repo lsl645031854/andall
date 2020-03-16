8a07b495dce85e76da1c0d50b6451c96e5e77a7d
shuailing.li <shuailing.li@andall.com>
2020-03-16 13:41:39 +0800

    fix

--- src/main/java/com/andall/sally/supply/config/CorsConfig.java
+++ src/main/java/com/andall/sally/supply/config/CorsConfig.java
@@ -1,41 +1,25 @@
-package com.andall.sally.supply.config;
-
-/*
-
-
-
-package com.zoma.cms.rest.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-
-
-
-
-@Configuration
-public class CorsConfig {
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
-        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
-        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
-        return corsConfiguration;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter(source);
-    }
-}
-
-
-
-
-*/
+//package com.andall.sally.supply.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+// //跨域配置
+//@Configuration
+//public class CorsConfig {
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+//        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+//        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); // 4
+//        return new CorsFilter(source);
+//    }
+//}
