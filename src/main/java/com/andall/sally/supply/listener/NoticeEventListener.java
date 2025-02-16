73181e448b10733a69c255ffb827ae5cd0b0eb10
lsl <645031854@qq.com>
2025-02-16 17:09:58 +0800

    哈哈哈

--- src/main/java/com/andall/sally/supply/listener/NoticeEventListener.java
+++ src/main/java/com/andall/sally/supply/listener/NoticeEventListener.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @Author: lsl 嘎嘎嘎
- * @Description: 多个监听器，触发可以有顺序， @order， 数值越小表示越优先
+ * @Description: 多个监听器，触发可以有顺序， @order， 数值越小表示越优先 哈哈哈
  * @Date: Created on 3:29 下午 2020/3/3
  */
 @Component
